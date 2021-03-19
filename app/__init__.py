96bbc976c10c4fcd1db1511b4d46ab895824a988
ethanschreur <ethanschreur@icloud.com>
2021-03-19 15:48:48 -0400

    setup shop page

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 """Initializes the flask app."""
-from flask import Flask
+from flask import Flask, render_template
 from models import connect_db
 from config import app_config
 
@@ -9,3 +9,7 @@ def create_app(config_name):
     app.config.from_object(app_config[config_name])
     connect_db(app)
     return app
+
+@app.route('/shop')
+def get_shop():
+    return render_template('customer/shop.html') 
